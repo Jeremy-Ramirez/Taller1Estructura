2226ca84814561d9350cb5dba32de731dfb6ddcb
Ronny-Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2020-10-28 22:43:20 -0500

    Problema #2
    DIANITA CRACK

--- Taller1/src/taller1/Taller1.java
+++ Taller1/src/taller1/Taller1.java
@@ -15,9 +15,10 @@ public class Taller1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        retornaPosicionF("aaaaa");
+        retornaPosicionF("aaaaa");//Prueba de problema #1
+        replaceOA("Buenos dias a todos!", 2);//Prueba de problema #2
     }
-    
+    //PROBLEMA #1...............................................................
     public static void retornaPosicionF(String s){
         retornaPosicion(s,0);
     }
@@ -36,4 +37,25 @@ public class Taller1 {
           
       
     }
+    
+    
+    //PROBLEMA #2...............................................................
+    public static void replaceOA(String cadena,int n){
+        replaceOA1(cadena,n,0);
+    }
+    
+    private static void replaceOA1(String s, int numero,int indice){
+        
+        if(numero==0){
+            System.out.println("palabra:  "+s);
+        }else if(indice==s.length()){
+            System.out.println("No");
+        }
+        else{
+            s=s.replaceFirst("o", "a");
+            replaceOA1(s,numero-1, indice+1);
+        } 
+        
+    }
+    
 }
